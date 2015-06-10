6d58573b18eb998d72e2950e9bc165b41880c15a
martylavender <mlavender@atlassian.com>
2015-06-10 17:55:32 -0500

    Committing fixes

--- CoreDataTest/ViewController.m
+++ CoreDataTest/ViewController.m
@@ -26,7 +26,7 @@
 #pragma mark - Properties
 
 - (NSManagedObjectContext *) managedObjectContext {
-    return [(AppDelegate *)[[UIApplication sharedApplication] delegate] managedObjectContext];    
+    return [(AppDelegate *)[[UIApplication sharedApplication] delegate] managedObjectContext];
 }
 
 -(NSFetchedResultsController *) fetchedResultsController {
@@ -87,103 +87,102 @@
 }
 
 #pragma mark - NSFetchedResultsControllerDelegate
-        
+
 - (void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
     //Lets the tableview know we're potentially doing a bunch of updates.
     [self.tableView beginUpdates];
 }
 
-<<<<<<< HEAD
+
+
+
 - (void)controller:(NSFetchedResultsController *)controller
-  didChangeObject:(id)anObject
-           atIndex:(NSIndexPath *)indexPath
-     forChangeType:(NSFetchedResultsChangeType)type
-      newIndexPath:(NSIndexPath *)newIndexPath {
-=======
-- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath {
-    
-    UITableView *tableView = self.tableView;
->>>>>>> 7d8e76d3dde2807643da1a9a616d049f1d65a225
-    
-    switch(type) {
-            
-        case NSFetchedResultsChangeInsert:
-            [tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
-            break;
-            
-        case NSFetchedResultsChangeDelete:
-            [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-            break;
-            
-        case NSFetchedResultsChangeUpdate:
-            [self configureCell:[tableView cellForRowAtIndexPath:indexPath] atIndexPath:indexPath];
-            break;
-            
-        case NSFetchedResultsChangeMove:
-            [tableView deleteRowsAtIndexPaths:[NSArray
-                                               arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-            [tableView insertRowsAtIndexPaths:[NSArray
-                                               arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
-            break;
-    }
-}
-
-
-- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id )sectionInfo atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type {
-    
-    switch(type) {
-            
-        case NSFetchedResultsChangeInsert:
-            [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
-            break;
-            
-        case NSFetchedResultsChangeDelete:
-            [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
-            break;
-            
-        case NSFetchedResultsChangeMove:
-        case NSFetchedResultsChangeUpdate:
-            break;
-    }
-}
-
-- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
-    //We're finished updating the tableview's data.
-    [self.tableView endUpdates];
-}
-
-#pragma mark - Actions
+didChangeObject:(id)anObject
+atIndexPath:(NSIndexPath *)indexPath
+forChangeType:(NSFetchedResultsChangeType)type
+newIndexPath:(NSIndexPath *)newIndexPath {
         
-- (IBAction)addItem:(id)sender {
-    if ([self.itemTextField.text isEqualToString:@""]) return;
-    
-    NSManagedObjectContext *context = self.managedObjectContext;
-    Item *newItem = [NSEntityDescription insertNewObjectForEntityForName:@"Item" inManagedObjectContext:context];
-    newItem.itemname = self.itemTextField.text;
-    self.itemTextField.text = @"";
-    NSError *error;
-    [context save:&error];
-    self.itemStatus.text = @"Item saved";
-}
-
-- (IBAction)findItem:(id)sender {
-    NSManagedObjectContext *context = self.managedObjectContext;
-    NSEntityDescription *entityDesc = [NSEntityDescription entityForName:@"Item" inManagedObjectContext:context];
-    NSFetchRequest *request = [[NSFetchRequest alloc] init];
-    [request setEntity:entityDesc];
-    NSPredicate *pred = [NSPredicate predicateWithFormat:@"(itemname = %@)", self.itemTextField.text];
-    [request setPredicate:pred];
-    
-    NSError *error;
-    NSArray *objects = [context executeFetchRequest:request error:&error];
-    
-    if ([objects count] == 0) {
-        self.itemStatus.text = @"No matches found";
-    } else {
-        Item *match = [objects firstObject];
-        self.itemTextField.text = match.itemname;
-        self.itemStatus.text = [NSString stringWithFormat: @"%lu matches found", (unsigned long)[objects count]];
+        UITableView *tableView = self.tableView;
+        
+        switch(type) {
+                
+            case NSFetchedResultsChangeInsert:
+                [tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+                break;
+                
+            case NSFetchedResultsChangeDelete:
+                [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+                break;
+                
+            case NSFetchedResultsChangeUpdate:
+                [self configureCell:[tableView cellForRowAtIndexPath:indexPath] atIndexPath:indexPath];
+                break;
+                
+            case NSFetchedResultsChangeMove:
+                [tableView deleteRowsAtIndexPaths:[NSArray
+                                                   arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+                [tableView insertRowsAtIndexPaths:[NSArray
+                                                   arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+                break;
+        }
     }
-}
 
-@end
+    
+    - (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id )sectionInfo atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type {
+        
+        switch(type) {
+                
+            case NSFetchedResultsChangeInsert:
+                [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+                break;
+                
+            case NSFetchedResultsChangeDelete:
+                [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+                break;
+                
+            case NSFetchedResultsChangeMove:
+            case NSFetchedResultsChangeUpdate:
+                break;
+        }
+    }
+    
+    - (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+        //We're finished updating the tableview's data.
+        [self.tableView endUpdates];
+    }
+    
+#pragma mark - Actions
+    
+    - (IBAction)addItem:(id)sender {
+        if ([self.itemTextField.text isEqualToString:@""]) return;
+        
+        NSManagedObjectContext *context = self.managedObjectContext;
+        Item *newItem = [NSEntityDescription insertNewObjectForEntityForName:@"Item" inManagedObjectContext:context];
+        newItem.itemname = self.itemTextField.text;
+        self.itemTextField.text = @"";
+        NSError *error;
+        [context save:&error];
+        self.itemStatus.text = @"Item saved";
+    }
+    
+    - (IBAction)findItem:(id)sender {
+        NSManagedObjectContext *context = self.managedObjectContext;
+        NSEntityDescription *entityDesc = [NSEntityDescription entityForName:@"Item" inManagedObjectContext:context];
+        NSFetchRequest *request = [[NSFetchRequest alloc] init];
+        [request setEntity:entityDesc];
+        NSPredicate *pred = [NSPredicate predicateWithFormat:@"(itemname = %@)", self.itemTextField.text];
+        [request setPredicate:pred];
+        
+        NSError *error;
+        NSArray *objects = [context executeFetchRequest:request error:&error];
+        
+        if ([objects count] == 0) {
+            self.itemStatus.text = @"No matches found";
+        } else {
+            Item *match = [objects firstObject];
+            self.itemTextField.text = match.itemname;
+            self.itemStatus.text = [NSString stringWithFormat: @"%lu matches found", (unsigned long)[objects count]];
+        }
+    }
+    
+    @end
